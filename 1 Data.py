bb7fd18f5a174c6c0a008da6c3682eb6a1dae958
laravnb <62602469+laravnb@users.noreply.github.com>
2024-05-16 12:25:17 +0200

    Update 1 Data.py
    Signed-off-by: laravnb <62602469+laravnb@users.noreply.github.com>

--- 1 Data.py
+++ 1 Data.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
 import glob
-from csv import writer, reader
 from datetime import datetime
 
-# download bitcoin options data from deribit
-# Specify directory path containing the CSV files, * for variations
-files_path = 'path/to/*-BTC-*MAY22-export.csv'
+### combines all the single data files to one big file, extracts variables,
+# imports volatility, converts market price to USD, cleans data
+
+# Specify directory path containing CSV file, * for variations
+files_path = 'Data/1Dataset/*-BTC-****22-export.csv'
 files = glob.glob(files_path) # glob to get list of matching files
 combined_df = pd.DataFrame()
 
@@ -19,14 +20,13 @@ for file in files:
     # Create a new column with the date
     df1['Date'] = date_from_file
 
-    # download bitcoin historical volatility from deribit or calculate it with their code
     # Read the historical value from another document based on the date
     vol_df = pd.read_csv('historical_volatility.csv')
     merged_df = pd.merge(df1, vol_df, on='Date', how='left')
     combined_df = combined_df.append(merged_df, ignore_index=True)
 
 # clearing all unnecessary columns (and removing NaN values)
-df2 = combined_df.drop(['Last', 'Size', 'IV (Bid)', 'IV (Ask)','Close'], axis = 1)
+df2 = combined_df.drop(['Last', 'Size', 'IV (Bid)', 'IV (Ask)'], axis = 1)
 df2 = df2.drop(['Size.1', 'Open', 'Δ|Delta', 'Gamma', 'Theta', 'Vega', 'Rho', 'Volume', ], axis = 1)
 # dropt all rows if there is a '-' value
 df2 = df2.replace('-', np.nan)
@@ -48,7 +48,7 @@ for i, row in df2.iterrows():
     option_types.append(option_type)
 
     # convert the 'Expiration' collumn to 3 decimal places
-    # and then convert it to years --> necessary for option pricing methods
+    #. and then convert it to years
     expiration = round(row['Expiration']/ 365, 3)
     expirations.append(expiration)
 
@@ -73,7 +73,6 @@ df2['Volatility'] = pd.to_numeric(df2['Volatility'])
 df2['Ask'] = pd.to_numeric(df2['Ask']) 
 df2['Bid'] = pd.to_numeric(df2['Bid']) 
 
-# dataset restrictions
 # Only keep in the money options and drop the other rows
 df2 = df2[(df2['Bitcoin Price'] > df2['Exercise Price']) & (df2['Option Type'] == 'C') | (df2['Bitcoin Price'] < df2['Exercise Price']) & (df2['Option Type'] == 'P')]
 df2['Spread_Percentage'] = ((df2['Ask'] - df2['Bid']) / df2['Ask']) * 100
@@ -82,7 +81,15 @@ df2 = df2[df2['Spread_Percentage'] < 50]
 df2 = df2.drop(['Spread_Percentage'], axis=1)
 # only keep options with expiration days between 5 and 20
 df2 = df2[(df2['Expiration'] >= 5/365) & (df2['Expiration'] <= 20/365)]
-df = df2.drop([ 'Bid', 'Ask'], axis=1)
 
-# Save the dataset for further use
-df.to_csv('final_data.csv', index=False)
+# Rearrange the rows based on the date
+df2 = df2.sort_values(by='Date', ascending=True)
+
+# Drop all unessecary collumns, add index and save to new file (put and call seperated)
+df = df2.drop([ 'Bid', 'Ask'], axis=1)
+df['Index'] = df.groupby('Option Type').cumcount() + 1
+df = df[['Index', 'Date', 'Instrument', 'Option Type', 'Bitcoin Price', 'Market Price', 'Expiration', 'Interest Rate', 'Exercise Price', 'Volatility']]
+df_p = df[df['Option Type'] == 'P']
+df_c = df[df['Option Type'] == 'C']
+df_c.to_csv('final_data_c.csv', index=False)
+df_p.to_csv('final_data_p.csv', index=False)
